93c645c825b3bd9977319b49312bb545a7bbb26d
nbross <nbross2001@gmail.com>
2022-05-07 01:04:03 -0400

    front end works now

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { animals } = require('./data/animals.json')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -94,6 +95,22 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
